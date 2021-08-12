4b22e5564464655415cf2a238e27fdcd7b8a1c4e
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2021-08-12 17:21:36 -0500

    Remove diagnostics on hover

--- nvim/lua/config/lsp/general.lua
+++ nvim/lua/config/lsp/general.lua
@@ -4,7 +4,7 @@ function M.setup(client, bufnr)
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-  vim.cmd [[autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()]]
+  -- vim.cmd [[autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()]]
   vim.cmd [[autocmd CursorHoldI * silent! lua vim.lsp.buf.signature_help()]]
 end
 
